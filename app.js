b55379dc3d0cb3598240d01a5554f4ccff9164ca
Conny1 <77896950+Conny1@users.noreply.github.com>
2022-08-12 12:51:53 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,296 @@
+const movies=[
+
+    {
+        img:"images/arcane.jpg",
+        Name:"Arcane",
+        category:"Animation",
+        rating:"7/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+    {
+        img:"images/bodyguard.jpg",
+        Name:"Bodyguard",
+        category:"Action",
+        rating:"9/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/bridgerton.jpg",
+        Name:"Bridgerton",
+        category:"romance",
+        rating:"5/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/creedii.jpg",
+        Name:"Creed ii",
+        category:"Action",
+        rating:"8/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/for.jpg",
+        Name:"FOR SAMA",
+        category:"Documentary",
+        rating:"7/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/hanna.jpg",
+        Name:"Hannah",
+        category:"Action",
+        rating:"10/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+    {
+        img:"images/hawkeye.jpg",
+        Name:"Hawkey",
+        category:"Sci-fi",
+        rating:"7/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/inception.jpg",
+        Name:"Inception",
+        category:"Action",
+        rating:"7/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+    {
+        img:"images/interstellar.jpg",
+        Name:"Interstellar",
+        category:"Sci-fi",
+        rating:"5/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/into.jpg",
+        Name:"Into the Mountain",
+        category:"Documentary",
+        rating:"6/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/joe.jpg",
+        Name:"J O E  B E L L",
+        category:"Documentary",
+        rating:"4/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/johnWick.jpg",
+        Name:"John Wick",
+        category:"Action",
+        rating:"10/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/manonaledge.jpg",
+        Name:"MAN ON A LEDGE",
+        category:"Action",
+        rating:"7/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+    {
+        img:"images/martian.jpg",
+        Name:"Martian",
+        category:"Sci-fi",
+        rating:"4/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/peterrabbit2.jpg",
+        Name:"Peter Rabbit 2",
+        category:"Animation",
+        rating:"5/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/prisonBreak.jpg",
+        Name:"Prison Break",
+        category:"Action",
+        rating:"9/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+    {
+        img:"images/RRR.jpg",
+        Name:"RRR",
+        category:"Action",
+        rating:"7/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+    {
+        img:"images/sander.jpg",
+        Name:"Sander",
+        category:"Action",
+        rating:"4/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+    {
+        img:"images/suits.jpg",
+        Name:"Suits",
+        category:"Drama",
+        rating:"7/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/Thecommando.jpg",
+        Name:"The Commando",
+        category:"Action",
+        rating:"8/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/thedefenders.jpg",
+        Name:"Defenders",
+        category:"Action",
+        rating:"7/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+    {
+        img:"images/thegreathack.jpg",
+        Name:"The great hack",
+        category:"Action",
+        rating:"7.5/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+    {
+        img:"images/theorder.jpg",
+        Name:"The Order",
+        category:"Sci-fi",
+        rating:"8.7/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/thestand.jpg",
+        Name:"The Stand",
+        category:"Action",
+        rating:"4/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/vikings.jpg",
+        Name:"Vikings",
+        category:"Action",
+        rating:"7/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/voyageoftime.jpg",
+        Name:"Voyage of time",
+        category:"Sci-fi",
+        rating:"6.5/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    },
+
+    {
+        img:"images/whiteCollar.jpg",
+        Name:"White collar",
+        category:"Drama",
+        rating:"7.5/10",
+        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iste recusandae reprehenderit minima asperiores. Aliquid alias omnis, laboriosam voluptatum eveniet voluptate, enim nesciunt dolores cum iusto voluptatibus sapiente, illum possimus?"
+    }
+
+]
+
+
+
+const content = document.querySelector('.content');
+
+
+const btnContainer = document.querySelector('.btn-container');
+
+
+window.addEventListener('DOMContentLoaded',()=>{
+   
+    loadcontent(movies)
+    // console.log(btns);
+   const dynamicbtn = movies.reduce((values,item)=>{
+
+    if (!values.includes(item.category)) {
+
+        values.push(item.category);
+        
+    }
+    return values;
+
+
+   },['All'])
+
+  let buttons = dynamicbtn.map(btn=>{
+    
+    return  `
+    <button data-id=${btn} class=" btn">${btn}</button>
+    `
+
+   }).join("");
+
+console.log(buttons);
+btnContainer.innerHTML = buttons;
+const btns =  document.querySelectorAll('.btn');
+btns.forEach(btn => {
+    
+    btn.addEventListener('click', (e)=>{
+        // console.log(e.currentTarget.dataset.id);
+      let  categories = e.currentTarget.dataset.id;
+       
+// filter the array based on datasets
+        let categoryitem = movies.filter((item)=>{
+            if(categories === item.category){
+                return item
+            }
+
+        });
+        // console.log(categoryitem);   
+        if(categories === 'All'){
+            loadcontent(movies);
+        }else{
+            loadcontent(categoryitem);
+        }    
+    });
+    
+});
+   
+});
+ 
+// function for loading content into the window
+function loadcontent(arr){
+    // console.log("hello");
+    let items = arr.map((item)=>{
+        // console.log(item)
+        return`
+        <article class="movie-container">
+        <div class="img-container"><img src=${item.img} alt=${item.Name} /></div>
+        <div class="description">
+          <p class="movie-name">Name: ${item.Name}</p>
+          <hr/>
+          <p class="category">category: ${item.category}</p>
+          <hr/>
+          <p class="movie-rating">imdb rating: ${item.rating}</p>
+          <hr/>
+          <p class="decription">
+          ${item.description}
+          </p>
+        </div>
+      </article> `
+
+    })
+   content.innerHTML= items.join('');
+   
+}
